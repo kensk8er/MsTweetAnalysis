1caed0a1638b25817d2bd1ab1631e1109caa359e
kensk8er <kensk8er1017@gmail.com>
2014-09-17 17:35:17 +0100

    perform wiki augmentation and collect more tweets

--- exec/LDA.py
+++ exec/LDA.py
@@ -7,7 +7,6 @@ import logging
 from gensim import corpora
 from gensim.models import LdaModel, TfidfModel
 from util.input import unpickle
-from util.log import configure_log
 from util.output import enpickle
 
 __author__ = 'kensk8er'
@@ -42,7 +41,7 @@ def perform_lda(dictionary, corpus, num_topics, wiki_path=None, passes=1, iterat
     return lda_model, doc_vectors
 
 
-def do_lda(num_topics, passes, iterations, chunksize, tfidf):
+def do_lda(num_topics, passes, iterations, chunksize, tfidf, wiki_path=None):
     model_name = 'tweets_'
 
     logging.info('Loading user dictionary...')
@@ -57,9 +56,12 @@ def do_lda(num_topics, passes, iterations, chunksize, tfidf):
         corpus = [tfidf_model[corpus] for corpus in corpus]
         model_name += 'tfidf_'
 
+    if wiki_path is not None:
+        model_name += 'wiki_'
+
     logging.info('Performing LDA on user corpus...')
     model, vectors = perform_lda(dictionary=dictionary, corpus=corpus, num_topics=num_topics, passes=passes,
-                                 iterations=iterations, chunksize=chunksize)
+                                 iterations=iterations, chunksize=chunksize, wiki_path=wiki_path)
     model.print_topics(topics=num_topics, topn=10)
     model.save('data/model/' + model_name + str(num_topics) + '.lda')
     enpickle(vectors, 'data/vector/' + model_name + str(num_topics) + '.pkl')
@@ -71,8 +73,9 @@ if __name__ == '__main__':
     passes = 2
     iterations = 50
     chunksize = 2000
+    wiki_path = 'data/processed/wikis.pkl'
 
     # logging
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
-    do_lda(num_topics=num_topics, passes=passes, iterations=iterations, chunksize=chunksize, tfidf=None)
+    do_lda(num_topics=num_topics, passes=passes, iterations=iterations, chunksize=chunksize, tfidf=None, wiki_path=wiki_path)
--- exec/getTweet.py
+++ exec/getTweet.py
@@ -1,32 +1,49 @@
+import csv
+import re
+from module.text.preprocessing import clean_text
+from util.input import unpickle
+
 __author__ = 'Linda Wang'
 
 import twitter
 import pandas as pd
 
-file_name = 'data/additional.csv'
 
-api = twitter.Api(
-    consumer_key='XrTlgFXqGt9XAnNFF7rec25wg',
-    consumer_secret='heORHeyXvbTK7RozVLLeUjDrsOZWSg75eUjewLT6FXh8UuLAaz',
-    access_token_key='2786813671-O1P8pWzrOAViJnZ0zkv3a4yl2rJHmbxcIfZUXub',
-    access_token_secret='c0S9NKtzXU9dyRrsVqura0EIzXFBIWro3rvJBkrCdSyJe')
+if __name__ == '__main__':
+    file_name = 'data/original/additional.csv'
+    location_file = 'data/original/MSShopLocations.csv'
 
-print api.VerifyCredentials()
-locations = pd.read_csv('/Users/lindawang/Desktop/sentiment', quotechar='"')
-locations['x'] = locations['Latitude/longitude'].str.split(',').apply(lambda x: x[0].strip())
-locations['y'] = locations['Latitude/longitude'].str.split(',').apply(lambda x: x[1].strip())
-del locations['Latitude/longitude']
+    api = twitter.Api(
+        consumer_key='XrTlgFXqGt9XAnNFF7rec25wg',
+        consumer_secret='heORHeyXvbTK7RozVLLeUjDrsOZWSg75eUjewLT6FXh8UuLAaz',
+        access_token_key='2786813671-O1P8pWzrOAViJnZ0zkv3a4yl2rJHmbxcIfZUXub',
+        access_token_secret='c0S9NKtzXU9dyRrsVqura0EIzXFBIWro3rvJBkrCdSyJe')
 
-tweets = []
-for i, row in enumerate(locations.iterrows()):
-    try:
-        print "{}th done".format(i)
-        res = api.GetSearch(geocode=(row[1].x, row[1].y, '0.1km'), count=100)
-        for tweet in res:
-            tweets.append(u'{},{},{}'.format(row[1].x, row[1].y, unicode(tweet.AsDict()['text']).replace('\n', '')))
-    except Exception as e:
-        print e
+    print api.VerifyCredentials()
+    # locations = pd.read_csv(location_file, quotechar='"')
+    # locations['x'] = locations['Latitude/longitude'].str.split(',').apply(lambda x: x[0].strip())
+    # locations['y'] = locations['Latitude/longitude'].str.split(',').apply(lambda x: x[1].strip())
+    # del locations['Latitude/longitude']
 
-with open(file_name, 'w+') as sink:
-    sink.write(u'\n'.join(tweets))
+    wikis = unpickle('data/others/wikis.pkl')
+    keywords = wikis.keys()
+    tweets = []
+    # for i, row in enumerate(locations.iterrows()):
+    for i, keyword in enumerate(keywords):
+        try:
+            print "{}th done".format(i)
+            # res = api.GetSearch(geocode=(row[1].x, row[1].y, '0.1km'), count=100)
+            res = api.GetSearch(term=keyword, lang='english', count=100)
+            for tweet in res:
+                # tweets.append(u'{},{},{}'.format(row[1].x, row[1].y, unicode(tweet.AsDict()['text']).replace('\n', '')))
+                tweets.append(unicode(tweet.AsDict()['text']).replace('\n', ''))
+        except Exception as e:
+            print e
 
+    f = open(file_name, 'ab')
+    csvWriter = csv.writer(f)
+    rx = re.compile('\W+')
+    for tweet in tweets:
+        tweet = clean_text(tweet)
+        tweet = rx.sub(' ', tweet).strip()
+        csvWriter.writerow([tweet])
